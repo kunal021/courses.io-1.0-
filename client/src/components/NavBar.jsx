55ab5f9f9ea3b73c6aa8eb22e48eb5cf66bb734e
Kunal Singh <ks820177@gmail.com>
2024-04-30 10:30:25 +0530

    update

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,52 +2,54 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="flex justify-between items-center m-6 mx-10 bg-black/30">
-      <div>
-        <p className="cursor-pointer">Courses.io</p>
+    <div className="bg-black/30">
+      <div className="flex justify-between items-center m-6 mx-10">
+        <div>
+          <p className="cursor-pointer">Courses.io</p>
+        </div>
+        <ul className="flex justify-center items-center space-x-6">
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${isActive ? "text-red-500" : "text-black"}`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/courses"}
+              className={({ isActive }) =>
+                `${isActive ? "text-red-500" : "text-black"}`
+              }
+            >
+              Courses
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/learning-path"}
+              className={({ isActive }) =>
+                `${isActive ? "text-red-500" : "text-black"}`
+              }
+            >
+              Learning Path
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/signup"}
+              className={({ isActive }) =>
+                `${isActive ? "text-red-500" : "text-black"}`
+              }
+            >
+              Sign Up
+            </NavLink>
+          </li>
+        </ul>
       </div>
-      <ul className="flex justify-center items-center space-x-6">
-        <li>
-          <NavLink
-            to={"/"}
-            className={({ isActive }) =>
-              `${isActive ? "text-red-500" : "text-black"}`
-            }
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={"/courses"}
-            className={({ isActive }) =>
-              `${isActive ? "text-red-500" : "text-black"}`
-            }
-          >
-            Courses
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={"/learning-path"}
-            className={({ isActive }) =>
-              `${isActive ? "text-red-500" : "text-black"}`
-            }
-          >
-            Learning Path
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={"/signup"}
-            className={({ isActive }) =>
-              `${isActive ? "text-red-500" : "text-black"}`
-            }
-          >
-            Sign Up
-          </NavLink>
-        </li>
-      </ul>
     </div>
   );
 }
