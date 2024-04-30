85d882fa476bf04b91f0c75d34575b03ec20b3dc
Kunal Singh <ks820177@gmail.com>
2024-04-30 08:38:50 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+// import DashBoard from "./components/DashBoard";
+// import TransferMoney from "./components/TransferMoney";
+import { AuthProvider } from "./AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+// import Test from "./Test";
+import Courses from "./components/Courses";
+import DataStructures from "./components/learning-path/DataStructures";
+import WebDevelopement from "./components/learning-path/WebDevelopement";
+import MachineLearning from "./components/learning-path/MachineLearning";
+// import Home from "./components/Home";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Test />} /> */}
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/data-structure" element={<DataStructures />} />
+          <Route path="/web-developement" element={<WebDevelopement />} />
+          <Route path="/machine-learning" element={<MachineLearning />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/dashboard" element={<PrivateRoute />}>
+            {/* <Route path="/dashboard" element={<DashBoard />} /> */}
+          </Route>
+          <Route path="/send" element={<PrivateRoute />}>
+            {/* <Route path="/send" element={<TransferMoney />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/Courses.jsx
@@ -0,0 +1,36 @@
+import course from "../data.json";
+
+function Courses() {
+  const courses = course.courses;
+  return (
+    <div className="flex flex-col">
+      <p className="mt-10 text-5xl font-black text-center">All Courses</p>
+      <div className="flex flex-wrap justify-between items-center h-full w-full">
+        {courses.map((item) => (
+          <div
+            key={item.id}
+            className="border-2 rounded-2xl border-gray-400 mx-10 my-14"
+          >
+            <div className="flex flex-col items-center">
+              <iframe
+                src={item.src}
+                title={item.title}
+                frameBorder={item.frameBorder}
+                allow={item.allow}
+                allowFullScreen={item.allowfullscreen}
+                height={280}
+                width={430}
+                className="rounded-t-2xl"
+              ></iframe>
+              <p className="m-4 text-3xl font-black text-center w-80 break-words">
+                {item.courseName}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Courses;
