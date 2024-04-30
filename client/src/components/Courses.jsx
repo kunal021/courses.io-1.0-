36c8bdc1293b1993256aa65f481dc522fbe1740c
Kunal Singh <ks820177@gmail.com>
2024-04-30 08:48:26 +0530

    update courses

--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -4,12 +4,12 @@ function Courses() {
   const courses = course.courses;
   return (
     <div className="flex flex-col">
-      <p className="mt-10 text-5xl font-black text-center">All Courses</p>
+      <p className="mt-10 text-3xl font-black text-center">All Courses</p>
       <div className="flex flex-wrap justify-between items-center h-full w-full">
         {courses.map((item) => (
           <div
             key={item.id}
-            className="border-2 rounded-2xl border-gray-400 mx-10 my-14"
+            className="border-2 rounded-xl border-gray-400 mx-10 my-14"
           >
             <div className="flex flex-col items-center">
               <iframe
@@ -18,11 +18,11 @@ function Courses() {
                 frameBorder={item.frameBorder}
                 allow={item.allow}
                 allowFullScreen={item.allowfullscreen}
-                height={280}
-                width={430}
-                className="rounded-t-2xl"
+                height={220}
+                width={380}
+                className="rounded-t-xl"
               ></iframe>
-              <p className="m-4 text-3xl font-black text-center w-80 break-words">
+              <p className="m-4 text-xl font-black text-center w-80 break-words">
                 {item.courseName}
               </p>
             </div>
