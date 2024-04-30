bb464af0a587eefdbf2928b034f139ec12d93b3a
Kunal Singh <ks820177@gmail.com>
2024-04-30 08:58:42 +0530

    update

--- client/src/components/learning-path/MachineLearning.jsx
+++ client/src/components/learning-path/MachineLearning.jsx
@@ -4,13 +4,13 @@ function MachineLearning() {
   const courses = course.courses;
   const MLCourse = courses.filter((item) => item.learningPath === 3);
   return (
-    <div className="flex flex-col">
-      <p className="mt-10 text-5xl font-black text-center">Machine Learning</p>
+    <div className="container flex flex-col">
+      <p className="mt-10 text-3xl font-black text-center">Machine Learning</p>
       <div className="flex flex-wrap justify-between items-center h-full w-full">
         {MLCourse.map((item) => (
           <div
             key={item.id}
-            className="border-2 rounded-2xl border-gray-400 mx-10 my-14"
+            className="border-2  rounded-xl border-gray-900 mx-6 my-14 hover:shadow-xl transition-all duration-300"
           >
             <div className="flex flex-col items-center">
               <iframe
@@ -19,11 +19,11 @@ function MachineLearning() {
                 frameBorder={item.frameBorder}
                 allow={item.allow}
                 allowFullScreen={item.allowfullscreen}
-                height={280}
-                width={430}
+                height={220}
+                width={350}
                 className="rounded-t-2xl"
               ></iframe>
-              <p className="m-4 text-3xl font-black text-center w-80 break-words">
+              <p className="m-4 text-xl font-black text-center w-80 break-words">
                 {item.courseName}
               </p>
             </div>
