5e9975e16ce29ea3622bb66a6c782481c9cfd7cf
Kunal Singh <ks820177@gmail.com>
2024-04-30 08:46:59 +0530

    chaged home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div className="text-center m-20 text-5xl font-black">Home</div>;
+  return (
+    <div className="text-center m-20 text-5xl font-black">
+      <Link to={"/courses"}>Courses</Link>
+    </div>
+  );
 }
 
 export default Home;
