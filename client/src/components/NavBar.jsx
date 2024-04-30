e2fc307c87d2c2b1398467fa52792abf3ac2da4d
Kunal Singh <ks820177@gmail.com>
2024-04-30 10:24:40 +0530

    update

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,22 +2,50 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center m-6 mx-10">
       <div>
-        <p>Courses.io</p>
+        <p className="cursor-pointer">Courses.io</p>
       </div>
-      <ul>
+      <ul className="flex justify-center items-center space-x-6">
         <li>
-          <NavLink>Home</NavLink>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              `${isActive ? "text-red-500" : "text-black"}`
+            }
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink>Courses</NavLink>
+          <NavLink
+            to={"/courses"}
+            className={({ isActive }) =>
+              `${isActive ? "text-red-500" : "text-black"}`
+            }
+          >
+            Courses
+          </NavLink>
         </li>
         <li>
-          <NavLink>Learning Path</NavLink>
+          <NavLink
+            to={"/learning-path"}
+            className={({ isActive }) =>
+              `${isActive ? "text-red-500" : "text-black"}`
+            }
+          >
+            Learning Path
+          </NavLink>
         </li>
         <li>
-          <NavLink>Sign Up</NavLink>
+          <NavLink
+            to={"/signup"}
+            className={({ isActive }) =>
+              `${isActive ? "text-red-500" : "text-black"}`
+            }
+          >
+            Sign Up
+          </NavLink>
         </li>
       </ul>
     </div>
